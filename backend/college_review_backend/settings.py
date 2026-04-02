08940930dc290d59f257b46a137759c3a1248f28
Muskan Tandel <muskantandel824@gmail.com>
2026-04-02 22:54:07 +0530

    Update settings.py

--- backend/college_review_backend/settings.py
+++ backend/college_review_backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-12345')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('43.205.119.74', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
